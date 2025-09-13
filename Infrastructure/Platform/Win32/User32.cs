279e853afd268910d3517a08598cea6a59b9a0f3
Stormsil <warframealex1204@gmail.com>
2025-09-13 19:02:42 +0300

    Начал рефакторинг

---
+++ Infrastructure/Platform/Win32/User32.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationCore.Infrastructure.Platform.Win32
+{
+    internal class User32
+    {
+    }
+}
